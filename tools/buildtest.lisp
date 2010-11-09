b9ca5c8ad8ff7399da55a1308a2639c43a921eb2
Joachim Fasting <joachim.fasting@gmail.com>
2010-11-09 04:06:16 +0100

    tools/buildtest.lisp: improved output suppression

--- tools/buildtest.lisp
+++ tools/buildtest.lisp
@@ -14,19 +14,19 @@
 ;; Load the system, signal error on failure.
 ;; Note that warnings and so on are ignored.
 ;; All other output is redirected to /dev/null.
+
 (defun exit (code)
   #+ccl (quit code)
   #+clisp (ext:exit code)
   #+ecl (ext:exit code)
   #+sbcl (sb-unix:unix-exit code))
 
-(with-open-file (*standard-output* "/dev/null" :direction :output :if-exists :append)
+(let* ((null-stream (make-broadcast-stream))
+       (*standard-output* null-stream)
+       (*trace-output* null-stream))
   (handler-case (asdf:operate 'asdf:load-op :webkit)
-    (error (c)
-           (progn
-             (format *error-output* "Failed to build system: ~A~%"
-                     c)
-             (exit 1)))))
+    (error (c) (progn (format *error-output* "Failed: ~A~%" c)
+                      (exit 1)))))
 
 ;; If we got here, everything went well
 (exit 0)
