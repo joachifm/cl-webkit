6d9cf77a49db65408f05fc97c25de6ed30b6dfd3
Joachim Fasting <joachifm@fastmail.fm>
2014-08-27 05:40:09 +0200

    Update tools/buildtest.lisp and git_hooks/pre-commit
    Adapt to webkit2 and recent SBCL

--- tools/buildtest.lisp
+++ tools/buildtest.lisp
@@ -1,6 +1,6 @@
-;; buildtest.lisp
-;;
-;; Commentary:
+;;; buildtest.lisp
+
+;;; Commentary:
 ;;
 ;; Attempt to load a system, report outcome to the OS.
 ;; Used in git_hooks/pre-commit
@@ -8,21 +8,15 @@
 ;; Usage:
 ;;
 ;;     $ lisp --eval '(progn (load "buildtest.lisp") (buildtest SYSTEM))'
-;;
-;; This assumes that SYSTEM's definition is in the CWD and that lisp has ASDF.
-;;
-;; Code:
 
-(defun exit (code)
-  #+ccl (quit code)
-  #+clisp (ext:exit code)
-  #+ecl (ext:exit code)
-  #+sbcl (sb-unix:unix-exit code))
+;;; Code:
+
+(in-package :cl-user)
 
 (eval-when (:load-toplevel :compile-toplevel)
   (unless (find-package :asdf)
     (format t "ASDF is required to run this script.")
-    (exit 1)))
+    (sb-ext:exit :code 1)))
 
 (defun buildtest (sys)
   (let* ((null-stream (make-broadcast-stream))
@@ -30,6 +24,5 @@
          (*trace-output* null-stream))
     (handler-case (asdf:operate 'asdf:load-op sys)
       (error (c) (progn (format *error-output* "Failed: ~A~%" c)
-                        (exit 1)))))
-
-  (exit 0))
+                        (sb-ext:exit :code 1)))))
+  (sb-ext:exit :code 0))
