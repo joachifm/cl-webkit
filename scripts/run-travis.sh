4c35abc493244b889b8ec27363727983203c1a53
Joachim Fasting <joachifm@fastmail.fm>
2014-09-27 19:32:42 +0200

    Initial attempt at travis config
    Currently, the travis script only performs a load test; at
    some point we'd want to run the test suite &c.

---
+++ scripts/run-travis.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+#### parameters
+
+SBCL_VER=1.2.3
+SBCL_ARCH=x86-64-linux
+SBCL_NAME="sbcl-${SBCL_VER}-${SBCL_ARCH}"
+SBCL_URL="http://prdownloads.sourceforge.net/sbcl/${SBCL_NAME}-binary.tar.bz2"
+
+LOCAL_LISP_TREE=${HOME}/common-lisp
+QL_HOME=${HOME}/quicklisp
+
+#### shell environment
+
+set -ev
+
+export SBCL_HOME="${HOME}/lib/sbcl"
+export PATH="${HOME}/bin${PATH:+:}${PATH}"
+export CL_SOURCE_REGISTRY="${TRAVIS_BUILD_DIR}//:${LOCAL_LISP_TREE}//:${CL_SOURCE_REGISTRY}"
+
+export XDG_DATA_HOME="${HOME}/.local/share"
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+mkdir -p "$XDG_DATA_HOME" "$XDG_CONFIG_HOME" "$XDG_CACHE_HOME"
+
+alias sbcl='sbcl --noinform --no-sysinit --no-userinit --disable-debugger'
+
+#### install system deps
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu trusty main restricted universe multiverse" -y
+sudo apt-get update -yy
+sudo apt-get install libgtk-3.0 libwebkit2gtk-3.0-dev
+
+#### bootstrap SBCL
+
+curl -# -L "${SBCL_URL}?use_mirror=autoselect" -o "${SBCL_NAME}.tar.bz2"
+bzip2 -dc "${SBCL_NAME}.tar.bz2" | tar -xf -
+(cd "$SBCL_NAME" && INSTALL_ROOT="$HOME" sh ./install.sh)
+
+#### bootstrap quicklisp
+
+curl -# -L http://beta.quicklisp.org/quicklisp.lisp -o ./quicklisp.lisp
+sbcl --load ./quicklisp.lisp --eval '(quicklisp-quickstart:install)' --quit
+
+#### install lisp dependencies
+
+mkdir -p "$LOCAL_LISP_TREE"
+git clone https://github.com/OdonataResearchLLC/lisp-unit.git ${LOCAL_LISP_TREE}/lisp-unit
+git clone https://github.com/crategus/cl-cffi-gtk.git ${LOCAL_LISP_TREE}/cl-cffi-gtk
+sbcl --load "${QL_HOME}/setup.lisp" --eval '(ql:quickload (list "cl-webkit2" "cl-webkit2-tests"))' --quit
+
+#### run
+# XXX: add run test suite and stuff here.
+
+exit 0
