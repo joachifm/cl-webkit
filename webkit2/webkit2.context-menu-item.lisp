d2e3234896419d7229dbee577404fe5bfc768361
Joachim Fasting <joachifm@fastmail.fm>
2014-10-14 12:41:21 +0200

    Bind WebkitContextMenuAction enum

--- webkit2/webkit2.context-menu-item.lisp
+++ webkit2/webkit2.context-menu-item.lisp
@@ -12,6 +12,52 @@
 
 (define-webkit-class "WebKitContextMenuItem" () ()) ; XXX: GInitiallyUnowned
 
+(define-g-enum "WebKitContextMenuAction" webkit-context-menu-action ()
+  :webkit-context-menu-action-no-action
+  :webkit-context-menu-action-open-link
+  :webkit-context-menu-action-open-link-in-new-window
+  :webkit-context-menu-action-download-link-to-disk
+  :webkit-context-menu-action-copy-link-to-clipboard
+  :webkit-context-menu-action-open-image-in-new-window
+  :webkit-context-menu-action-download-image-to-disk
+  :webkit-context-menu-action-copy-image-to-clipboard
+  :webkit-context-menu-action-copy-image-url-to-clipboard
+  :webkit-context-menu-action-open-frame-in-new-window
+  :webkit-context-menu-action-go-back
+  :webkit-context-menu-action-go-forward
+  :webkit-context-menu-action-stop
+  :webkit-context-menu-action-reload
+  :webkit-context-menu-action-copy
+  :webkit-context-menu-action-cut
+  :webkit-context-menu-action-paste
+  :webkit-context-menu-action-delete
+  :webkit-context-menu-action-select-all
+  :webkit-context-menu-action-input-methods
+  :webkit-context-menu-action-unicode
+  :webkit-context-menu-action-spelling-guess
+  :webkit-context-menu-action-no-guesses-found
+  :webkit-context-menu-action-ignore-spelling
+  :webkit-context-menu-action-learn-spelling
+  :webkit-context-menu-action-ignore-grammar
+  :webkit-context-menu-action-font-menu
+  :webkit-context-menu-action-bold
+  :webkit-context-menu-action-italic
+  :webkit-context-menu-action-underline
+  :webkit-context-menu-action-outline
+  :webkit-context-menu-action-inspect-element
+  :webkit-context-menu-action-open-video-in-new-window
+  :webkit-context-menu-action-open-audio-in-new-window
+  :webkit-context-menu-action-copy-video-link-to-clipboard
+  :webkit-context-menu-action-copy-audio-link-to-clipboard
+  :webkit-context-menu-action-toggle-media-controls
+  :webkit-context-menu-action-toggle-media-loop
+  :webkit-context-menu-action-enter-video-fullscreen
+  :webkit-context-menu-action-media-pause
+  :webkit-context-menu-action-media-mute
+  :webkit-context-menu-action-download-video-to-disk
+  :webkit-context-menu-action-download-audio-to-disk
+  :webkit-context-menu-action-action-custom)
+
 (defcfun "webkit_context_menu_item_new" (g-object webkit-context-menu-item)
   (action (g-object gtk:gtk-action)))
 (export 'webkit-context-menu-item-new)
