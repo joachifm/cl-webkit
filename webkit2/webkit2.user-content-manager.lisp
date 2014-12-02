d27912d2519f2f7ee7ec835674ac9d0f87a12798
Joachim Fasting <joachifm@fastmail.fm>
2014-12-02 20:18:33 +0100

    Add webkit2.user-content-manager.lisp
    Only exposes the user content manager type itself.

---
+++ webkit2/webkit2.user-content-manager.lisp
@@ -0,0 +1,13 @@
+;;; webkit2.user-content-manager.lisp --- bindings for WebKitUserContentManager
+
+;; This file is part of cl-webkit.
+;;
+;; cl-webkit is free software; you can redistribute it and/or modify
+;; it under the terms of the MIT license.
+;; See `COPYING' in the source distribution for details.
+
+;;; Code:
+
+(in-package :webkit2)
+
+(define-webkit-class "WebKitUserContentManager" () ())
