7d7432955c733697b5d8d22211ac62e865761502
Joachim Fasting <joachifm@fastmail.fm>
2014-12-08 23:41:48 +0100

    Initial FFI binding to libsoup

---
+++ soup/init.lisp
@@ -0,0 +1,17 @@
+;;; init.lisp --- initialize foreign library
+
+;; This file is part of cl-webkit.
+;;
+;; cl-webkit is free software; you can redistribute it and/or modify
+;; it under the terms of the MIT license.
+;; See `COPYING' in the source distribution for details.
+
+;;; Code:
+
+(in-package #:cl-soup)
+
+(define-foreign-library libsoup
+    (:unix (:or "libsoup-2.4.so.1.7.0" ; libsoup-2.48.0
+                )))
+
+(use-foreign-library libsoup)
---
+++ soup/message-headers.lisp
@@ -0,0 +1,32 @@
+;;;; message-headers.lisp -- binds SoupMessageHeaders
+
+;; This file is part of cl-webkit.
+;;
+;; cl-webkit is free software; you can redistribute it and/or modify
+;; it under the terms of the MIT license.
+;; See `COPYING' in the source distribution for details.
+
+;;; Code:
+
+(in-package #:cl-soup)
+
+(defctype soup-message-headers :pointer)
+
+(defcfun "soup_message_headers_get_one" :string
+  (headers soup-message-headers)
+  (name :string))
+
+(defcfun "soup_message_headers_get_list" :string
+  (headers soup-message-headers)
+  (name :string))
+
+;;; libsoup provides a deprecated function of the same name; users
+;;; of this binding are unlikely to care whether the header they are
+;;; getting can occur only one, so provide a unified interface.
+;;;
+;;; Ideally, we'd have a soup-message-headers->hashtable function.
+(defun soup-message-headers-get (headers name)
+  (or (soup-message-headers-get-one headers name)
+      (soup-message-headers-get-list headers name)))
+
+(export 'soup-message-headers-get)
