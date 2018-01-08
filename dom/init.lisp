d76686790c91b5b2050329df487eb9a2361dc66b
John Mercouris <john@mercouris.email>
2018-01-08 11:59:49 -0600

    though dom functions are most definitely not available all versions of webkit, add them to avoid compilation errors

--- dom/init.lisp
+++ dom/init.lisp
@@ -11,6 +11,10 @@
 (in-package #:webkit-dom)
 
 (define-foreign-library libwebkit2
-    (:unix (:or "libwebkit2gtk-3.0.so")))
+    (:unix (:or "libwebkit2gtk-4.0.so"         ; webkit2gtk-2.6.4
+                "libwebkit2gtk-3.0.so.25.10.9" ; webkit2gtk-2.4.6
+                "libwebkit2gtk-3.0.so.25.10.8" ; webkit2gtk-2.4.5
+                "libwebkit2gtk-3.0.so.25.10.7" ; webkit2gtk-2.4.4
+                "libwebkit2gtk-3.0.so.25")))
 
 (use-foreign-library libwebkit2)
