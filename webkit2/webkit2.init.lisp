11399d50f89707267b3b18f50b6e4d87a1c5b541
John Mercouris <john@mercouris.email>
2020-05-19 17:15:21 +0200

    webkit2.init.lisp: init same time as cl-cffi-gtk

--- webkit2/webkit2.init.lisp
+++ webkit2/webkit2.init.lisp
@@ -10,18 +10,18 @@
 
 (in-package #:webkit2)
 
-(define-foreign-library libwebkit2
-  (:darwin (:or
-            "libwebkit2gtk-4.0.37.39.3.dylib"
-            "libwebkit2gtk-4.0.37.dylib"
-            "libwebkit2gtk-4.0.dylib"))
-  (:unix (:or "libwebkit2gtk-4.0.so"         ; webkit2gtk-2.6.4
-              "libwebkit2gtk-3.0.so.25.10.9" ; webkit2gtk-2.4.6
-              "libwebkit2gtk-3.0.so.25.10.8" ; webkit2gtk-2.4.5
-              "libwebkit2gtk-3.0.so.25.10.7" ; webkit2gtk-2.4.4
-              "libwebkit2gtk-3.0.so.25")))
-
-(use-foreign-library libwebkit2)
+(glib::at-init ()
+  (define-foreign-library libwebkit2
+    (:darwin (:or
+              "libwebkit2gtk-4.0.37.39.3.dylib"
+              "libwebkit2gtk-4.0.37.dylib"
+              "libwebkit2gtk-4.0.dylib"))
+    (:unix (:or "libwebkit2gtk-4.0.so"         ; webkit2gtk-2.6.4
+                "libwebkit2gtk-3.0.so.25.10.9" ; webkit2gtk-2.4.6
+                "libwebkit2gtk-3.0.so.25.10.8" ; webkit2gtk-2.4.5
+                "libwebkit2gtk-3.0.so.25.10.7" ; webkit2gtk-2.4.4
+                "libwebkit2gtk-3.0.so.25")))
+  (use-foreign-library libwebkit2))
 
 (defcfun "webkit_get_major_version" :int)
 (defcfun "webkit_get_minor_version" :int)
