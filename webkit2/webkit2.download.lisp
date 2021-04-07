db855639d4a13f6ba296959cf11635b6b67421bf
Pierre Neidhardt <mail@ambrevar.xyz>
2021-04-07 14:19:14 +0200

    webkit2.download.lisp: Add rest of the methods.

--- webkit2/webkit2.download.lisp
+++ webkit2/webkit2.download.lisp
@@ -18,11 +18,48 @@
    ("response" "WebKitURIResponse")
    ("allow-overwrite" "gboolean")))
 
+(defcfun "webkit_download_get_request" (g-object webkit-uri-request)
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-request)
+
+(defcfun "webkit_download_get_destination" :pointer ; const gchar*
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-destination)
+
 (defcfun "webkit_download_set_destination" :void
   (webkit-download (g-object webkit-download))
   (uri :string))
 (export 'webkit-download-set-destination)
 
+(defcfun "webkit_download_get_response" (g-object webkit-uri-request)
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-response)
+
 (defcfun "webkit_download_cancel" :void
   (webkit-download (g-object webkit-download)))
 (export 'webkit-download-cancel)
+
+(defcfun "webkit_download_get_estimated_progress" :double
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-estimated-progress)
+
+(defcfun "webkit_download_get_elapsed_time" :double
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-elapsed-time)
+
+(defcfun "webkit_download_get_received_data_length" :uint ; guint64
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-received-data-length)
+
+(defcfun "webkit_download_get_web_view" (g-object webkit-web-view)
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-web-view)
+
+(defcfun "webkit_download_get_allow_overwrite" :boolean
+  (webkit-download (g-object webkit-download)))
+(export 'webkit-download-get-allow-overwrite)
+
+(defcfun "webkit_download_set_allow_overwrite" :void
+  (webkit-download (g-object webkit-download))
+  (allowed :boolean))
+(export 'webkit-download-set-allow-overwrite)
