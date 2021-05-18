04250aac4ddb83765b482b452bb02756df10c631
John Mercouris <john@mercouris.email>
2021-05-18 11:43:22 +0200

    webkit2/webkit2.uri-scheme-request.lisp: simplify log

--- webkit2/webkit2.uri-scheme-request.lisp
+++ webkit2/webkit2.uri-scheme-request.lisp
@@ -72,7 +72,7 @@
                 (cffi:foreign-string-free ffi-string)))
           (error (c)
             (webkit-uri-scheme-request-finish-error
-             request (format nil "The custom url request for URI ~a failed with ~a: ~a"
+             request (format nil "The custom request for URI ~a failed with ~a: ~a"
                              (webkit-uri-scheme-request-get-uri request) (type-of c) c))
             (when (and callback (callback-error-function callback))
               (funcall (callback-error-function callback) c))))))))
