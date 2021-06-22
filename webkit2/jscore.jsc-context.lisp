a85ca91c006bc4941f5114f5fcc6a90d156cbbb8
Artyom Bologov <aartaka@protonmail.com>
2021-06-22 18:09:07 +0500

    Add file headers for JSCore files.

--- webkit2/jscore.jsc-context.lisp
+++ webkit2/jscore.jsc-context.lisp
@@ -1,3 +1,13 @@
+;;; jscore.jsc-context.lisp --- bindings for JSCContext
+
+;; This file is part of cl-webkit.
+;;
+;; cl-webkit is free software; you can redistribute it and/or modify
+;; it under the terms of the MIT license.
+;; See `COPYING' in the source distribution for details.
+
+;;; Code:
+
 (in-package #:webkit2)
 
 (define-webkit-class "JSCContext"
