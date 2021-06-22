7f5c4879806ef12a1e5ff15f5be17425d43ce23e
Artyom Bologov <aartaka@protonmail.com>
2021-06-22 18:09:07 +0500

    Add file headers for JSCore files.

--- webkit2/jscore.jsc-class.lisp
+++ webkit2/jscore.jsc-class.lisp
@@ -1,3 +1,13 @@
+;;; jscore.jsc-class.lisp --- bindings for JSCClass
+
+;; This file is part of cl-webkit.
+;;
+;; cl-webkit is free software; you can redistribute it and/or modify
+;; it under the terms of the MIT license.
+;; See `COPYING' in the source distribution for details.
+
+;;; Code:
+
 (in-package #:webkit2)
 
 (define-webkit-class "JSCClass"
