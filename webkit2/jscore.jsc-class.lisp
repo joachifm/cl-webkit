18d91956ba4c164a015ae1716412e21c7e6e3879
Artyom Bologov <aartaka@protonmail.com>
2021-07-08 17:33:06 +0500

    jsc-class: Bind all functions.

--- webkit2/jscore.jsc-class.lisp
+++ webkit2/jscore.jsc-class.lisp
@@ -15,3 +15,60 @@
     (("context" "JSCContext")
      ("name" "gchararray")
      ("parent" "JSCClass")))
+
+(defcfun "jsc_class_get_name" :string
+  (class (g-object jsc-class)))
+(export 'jsc-class-get-name)
+(defcfun "jsc_class_get_parent" (g-object jsc-class)
+  (class (g-object jsc-class)))
+(export 'jsc-class-get-parent)
+
+(defcfun "jsc_class_add_constructorv" (g-object jsc-value)
+  (class (g-object jsc-class))
+  (name :string)
+  (callback :pointer) ;; XXX: GCallback
+  (user-data :pointer)
+  (destroy-notify :pointer) ;; XXX: GDestroyNotify
+  (return-type :pointer) ;; XXX: GType
+  (n-parameters :uint)
+  (parameter-types (:pointer :pointer)))
+(export 'jsc-class-add-constructorv)
+
+(defcfun "jsc_class_add_constructor_variadic" (g-object jsc-value)
+  (class (g-object jsc-class))
+  (name :string)
+  (callback :pointer) ;; XXX: GCallback
+  (user-data :pointer)
+  (destroy-notify :pointer) ;; XXX: GDestroyNotify
+  (return-type :pointer)) ;; XXX: GType
+(export 'jsc-class-add-constructor-variadic)
+
+(defcfun "jsc_class_add_methodv" (g-object jsc-value)
+  (class (g-object jsc-class))
+  (name :string)
+  (callback :pointer) ;; XXX: GCallback
+  (user-data :pointer)
+  (destroy-notify :pointer) ;; XXX: GDestroyNotify
+  (return-type :pointer) ;; XXX: GType
+  (n-parameters :uint)
+  (parameter-types (:pointer :pointer)))
+(export 'jsc-class-add-methodv)
+
+(defcfun "jsc_class_add_method_variadic" (g-object jsc-value)
+  (class (g-object jsc-class))
+  (name :string)
+  (callback :pointer) ;; XXX: GCallback
+  (user-data :pointer)
+  (destroy-notify :pointer) ;; XXX: GDestroyNotify
+  (return-type :pointer)) ;; XXX: GType
+(export 'jsc-class-add-method-variadic)
+
+(defcfun "jsc_class_add_property" :void
+  (class (g-object jsc-class))
+  (name :string)
+  (property-type :pointer) ;; XXX: GType
+  (getter :pointer) ;; XXX: GCallback
+  (setter :pointer) ;; XXX: GCallback
+  (user-data :pointer)
+  (destroy-notify :pointer)) ;; XXX: GDestroyNotify
+(export 'jsc-class-add-property)
