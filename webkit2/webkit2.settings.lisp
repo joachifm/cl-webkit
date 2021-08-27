dbf9c40157abcdb0c81b96ab08997056393b63d8
Artyom Bologov <aartaka@protonmail.com>
2021-08-27 17:36:04 +0500

    settings: (Most probably) complete bindings.

--- webkit2/webkit2.settings.lisp
+++ webkit2/webkit2.settings.lisp
@@ -11,7 +11,10 @@
 (in-package #:webkit2)
 
 (define-webkit-class "WebKitSettings" ()
-   (("allow-modal-dialogs" "gboolean" t t)
+   (("allow-file-access-from-file-urls" "gboolean" t t)
+    ("allow-modal-dialogs" "gboolean" t t)
+    ("allow-universal-access-from-file-urls" "gboolean" t t)
+    ("allow-top-navigation-to-data-urls" "gboolean" t t)
     ("auto-load-images" "gboolean" t t)
     ("cursive-font-family" "gchararray" t t)
     ("default-charset" "gchararray" t t)
@@ -20,9 +23,11 @@
     ("default-monospace-font-size" "guint" t t)
     ("draw-compositing-indicators" "gboolean" t t)
     ("enable-accelerated-2d-canvas" "gboolean" t t)
+    ("enable-back-forward-navigation-gestures" "gboolean" t t)
     ("enable-caret-browsing" "gboolean" t t)
     ("enable-developer-extras" "gboolean" t t)
     ("enable-dns-prefetching" "gboolean" t t)
+    ("enable-encrypted-media" "gboolean" t t)
     ("enable-frame-flattening" "gboolean" t t)
     ("enable-fullscreen" "gboolean" t t)
     ("enable-html5-database" "gboolean" t t)
@@ -31,8 +36,10 @@
     ("enable-java" "gboolean" t t)
     ("enable-javascript" "gboolean" t t)
     ("enable-javascript-markup" "gboolean" t t)
+    ("enable-media-capabilities" "gboolean" t t)
     ("enable-media-stream" "gboolean" t t)
     ("enable-mediasource" "gboolean" t t)
+    ("enable-mock-capture-devices" "gboolean" t t)
     ("enable-offline-web-application-cache" "gboolean" t t)
     ("enable-page-cache" "gboolean" t t)
     ("enable-plugins" "gboolean" t t)
@@ -55,9 +62,35 @@
     ("minimum-font-size" "guint" t t)
     ("monospace-font-family" "gchararray" t t)
     ("pictograph-font-family" "gchararray" t t)
+    ("print-backgrounds" "gboolean" t t)
     ("sans-serif-font-family" "gchararray" t t)
     ("serif-font-family" "gchararray" t t)
     ("user-agent" "gchararray" t t)
     ("zoom-text-only" "gboolean" t t)
     #+webkit2-media
     ("enable-media" "gboolean" t t)))
+
+(define-g-enum "WebKitHardwareAccelerationPolicy"
+    webkit-hardware-acceleration-policy ()
+  :webkit-hardware-acceleration-policy-on-demand
+  :webkit-hardware-acceleration-policy-always
+  :webkit-hardware-acceleration-policy-never)
+
+(defcfun "webkit_settings_get_hardware_acceleration_policy"
+    webkit-hardware-acceleration-policy
+  (settings (g-object webkit-settings)))
+(export 'webkit-settings-get-hardware-acceleration-policy)
+
+(defcfun "webkit_settings_set_hardware_acceleration_policy"
+    :void
+  (settings (g-object webkit-settings))
+  (policy webkit-hardware-acceleration-policy))
+(export 'webkit-settings-set-hardware-acceleration-policy)
+
+(defcfun "webkit_settings_font_size_to_points" :uint
+  (pixels :uint))
+(export 'webkit-settings-font-size-to-points)
+
+(defcfun "webkit_settings_font_size_to_pixels" :uint
+  (points :uint))
+(export 'webkit-settings-font-size-to-pixels)
