dc730eb6c7e5b1e9da774372b08da8c70c02dd02
Artyom Bologov <aartaka@protonmail.com>
2021-09-21 01:02:15 +0500

    permission-request: Add all the WebKit types of permission requests.

--- webkit2/webkit2.permission-request.lisp
+++ webkit2/webkit2.permission-request.lisp
@@ -21,3 +21,53 @@
 (defcfun "webkit_permission_request_deny" :void
   (permission-request (g-object webkit-permission-request)))
 (export 'webkit-permission-request-deny)
+
+(define-webkit-class "WebKitGeolocationPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(define-webkit-class "WebKitInstallMissingMediaPluginsPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(defcfun "webkit_install_missing_media_plugins_permission_request_get_description" :string
+  (request (g-object webkit-install-media-plugins-permission-request)))
+(export 'webkit-install-missing-media-plugins-permission-request-get-description)
+
+(define-webkit-class "WebKitMediaKeySystemPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(defcfun "webkit_media_key_system_permission_get_name" :string
+  (request (g-object webkit-media-key-system-permission-request)))
+(export 'webkit-media-key-system-permission-get-name)
+
+(define-webkit-class "WebKitUserMediaPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest"))
+    (("is-for-audio-device" "gboolean")
+     ("is-for-video-device" "gboolean")))
+
+(defcfun "webkit_user_media_permission_is_for_audio_device" :boolean
+  (request (g-object webkit-user-media-permission-request)))
+(export 'webkit-user-media-permission-is-for-audio-device)
+
+(defcfun "webkit_user_media_permission_is_for_video_device" :boolean
+  (request (g-object webkit-user-media-permission-request)))
+(export 'webkit-user-media-permission-is-for-video-device)
+
+(define-webkit-class "WebKitDeviceInfoPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(define-webkit-class "WebKitNotificationPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(define-webkit-class "WebKitPointerLockPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(define-webkit-class "WebKitWebsiteDataAccessPermissionRequest"
+    (:interfaces ("WebKitPermissionRequest")) ())
+
+(defcfun "webkit_website_data_access_permission_request_get_requesting_domain" :string
+  (request (g-object webkit-website-data-access-permission-request)))
+(export 'webkit-website-data-access-permission-request-get-requesting-domain)
+
+(defcfun "webkit_website_data_access_permission_request_get_current_domain" :string
+  (request (g-object webkit-website-data-access-permission-request)))
+(export 'webkit-website-data-access-permission-request-get-current-domain)
