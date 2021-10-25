40b883987664352222ce0fe51e4d0473cf5ec106
Artyom Bologov <aartaka@protonmail.com>
2021-10-25 19:16:55 +0500

    util: Add g-variant-get-maybe-string.

--- webkit2/util.lisp
+++ webkit2/util.lisp
@@ -15,3 +15,16 @@
   (let ((s (apply #'concatenate 'string (mapcar #'string lst))))
     (intern (string-upcase s)
             (load-time-value (find-package ':cl-webkit2)))))
+
+(defun g-variant-get-maybe-string (variant)
+  "Get a string out of the VARIANT.
+If it's NULL, return nil."
+  (cond
+    ((null variant) nil)
+    ((cffi:null-pointer-p variant) nil)
+    ((glib:g-variant-type-is-maybe (glib:g-variant-get-type variant))
+     (let ((maybe (cffi:foreign-funcall "g_variant_get_maybe" (:pointer (:struct glib:g-variant))
+                                        variant (:pointer (:struct glib:g-variant)))))
+       (unless (or (null maybe) (cffi:null-pointer-p maybe))
+         (glib:g-variant-get-string maybe))))
+    (t (glib:g-variant-get-string variant))))
