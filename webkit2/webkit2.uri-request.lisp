dbbfe3c0972c1c83bbb064cca3d477e1bfd88bff
Artyom Bologov <aartaka@protonmail.com>
2021-11-09 15:28:41 +0500

    uri-request: Add full WebKitURIRequest bindings.

--- webkit2/webkit2.uri-request.lisp
+++ webkit2/webkit2.uri-request.lisp
@@ -12,3 +12,24 @@
 
 (define-webkit-class "WebKitURIRequest" ()
   (("uri" "gchararray" t t)))
+
+(defcfun "webkit_uri_request_new" (g-object webkit-uri-request)
+  (uri :string))
+(export 'webkit-uri-request-new)
+
+(defcfun "webkit_uri_request_get_uri" :string
+  (request (g-object webkit-uri-request)))
+(export 'webkit-uri-request-get-uri)
+
+(defcfun "webkit_uri_request_set_uri" :void
+  (request (g-object webkit-uri-request))
+  (uri :string))
+(export 'webkit-uri-request-set-uri)
+
+(defcfun "webkit_uri_request_get_http_method" :string
+  (request (g-object webkit-uri-request)))
+(export 'webkit-uri-request-get-http-method)
+
+(defcfun "webkit_uri_request_get_http_headers" :pointer
+  (request (g-object webkit-uri-request)))
+(export 'webkit-uri-request-get-http-headers)
