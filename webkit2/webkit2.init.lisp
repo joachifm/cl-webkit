09fe363e49718e1e66b2b928da21e820d4e2995c
aartaka <aartaka@protonmail.com>
2022-05-03 16:45:15 +0500

    init: Add webkit 5.0 as an option for a library.

--- webkit2/webkit2.init.lisp
+++ webkit2/webkit2.init.lisp
@@ -13,14 +13,17 @@
 (glib::at-init ()
   (define-foreign-library libwebkit2
     (:darwin (:or
+              "libwebkit2gtk-5.0.dylib"
               "libwebkit2gtk-4.1.dylib"
               "libwebkit2gtk-4.0.37.39.3.dylib"
               "libwebkit2gtk-4.0.37.dylib"
               "libwebkit2gtk-4.0.dylib"))
-    (:unix (:or "libwebkit2gtk-4.1.so"
-                "libwebkit2gtk-4.0.so"
-                ;; Fedora only has this one?
-                "libwebkit2gtk-4.0.so.37")))
+    (:unix (:or
+            "libwebkit2gtk-5.0.so"
+            "libwebkit2gtk-4.1.so"
+            "libwebkit2gtk-4.0.so"
+            ;; Fedora only has this one?
+            "libwebkit2gtk-4.0.so.37")))
   (use-foreign-library libwebkit2))
 
 (defcfun "webkit_get_major_version" :int)
