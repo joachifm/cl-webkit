623efe3128937c68341f852e75e21bbbbde7d6f0
Andre A. Gomes <andremegafone@gmail.com>
2024-07-22 18:28:43 +0300

    web-view(webkit-web-view-evaluate-javascript): Fix type signature.

--- webkit2/webkit2.web-view.lisp
+++ webkit2/webkit2.web-view.lisp
@@ -276,7 +276,8 @@
 
 (declaim (ftype (function (webkit-web-view string &optional
                                            (or null (function (t t)))
-                                           (or null (function (condition))) string))
+                                           (or null (function (condition)))
+                                           (or null string)))
                 webkit-web-view-evaluate-javascript))
 (defun webkit-web-view-evaluate-javascript (web-view javascript
                                             &optional call-back error-call-back world)
